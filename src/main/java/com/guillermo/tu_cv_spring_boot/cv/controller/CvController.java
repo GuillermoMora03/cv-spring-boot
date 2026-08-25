78f5b45b619f15a44e96b0aa3ae225dba6176c21
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-25 10:43:27 -0600

    feat: agregar persona dentro del controlador e index

--- src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/CvController.java
+++ src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/CvController.java
@@ -1,6 +1,8 @@
 package com.guillermo.tu_cv_spring_boot.cv.controller;
 
+import com.guillermo.tu_cv_spring_boot.cv.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/cv")
 public class CvController {
 
-    @GetMapping("/home")
-    public String index() {
+    @GetMapping({"/index", "", "/"})
+    public String index(Model model) {
+        Person person = new Person("Gabriel", "Chaldú", "Dev");
+        model.addAttribute("name", "Ga");
+        model.addAttribute("persona", person);
         return "index";
     }
 }
