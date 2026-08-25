038279e0a70393f8df73982a8dd595c6ce72c73d
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-25 10:06:41 -0600

    feat: agregar RequestMapping a controller

--- src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/CvController.java
+++ src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/CvController.java
@@ -2,8 +2,10 @@ package com.guillermo.tu_cv_spring_boot.cv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cv")
 public class CvController {
 
     @GetMapping("/home")
