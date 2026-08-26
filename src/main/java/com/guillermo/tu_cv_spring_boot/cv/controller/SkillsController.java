801469034b218b9f4d28ea1c098b5bc7aa6e1c49
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-26 11:17:18 -0600

    feat: agregar pathVariable y RequestParam en controller, crear form de busqueda en skill.html

--- src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/SkillsController.java
+++ src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -3,10 +3,7 @@ package com.guillermo.tu_cv_spring_boot.cv.controller;
 import com.guillermo.tu_cv_spring_boot.cv.model.Skill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +14,40 @@ public class SkillsController {
 
     private final List<Skill> skills = new ArrayList<>();
 
+    //Implementación Tradicional con model
+//    @GetMapping
+//    public String showSkills(Model model) {
+//        model.addAttribute("skills", skills);
+//        return "skills";
+//    }
+
     @GetMapping
-    public String showSkills(Model model) {
-        model.addAttribute("skills", skills);
+    public String showSkills(@RequestParam(defaultValue = "", required = false) String filter, Model model) {
+        List<Skill> skillsFilter = skills.stream()
+                        .filter(skill -> skill.getName().toLowerCase().contains(filter.toLowerCase()))
+                        .toList();
+
+        model.addAttribute("skills", skillsFilter);
+        model.addAttribute("filter", filter);
         return "skills";
     }
+
+    @GetMapping("/{index}")
+    public String showSkillDetail(@PathVariable int index, Model model){
+        if(index >= 0 && index < skills.size()){
+            Skill skill = skills.get(index);
+            model.addAttribute("skill", skill);
+            return "skill-detail";
+        }
+
+        return "redirect:/skills";
+    }
+
+    //Implementación con modelAttribute
+//    @ModelAttribute(name = "skills2")
+//    public List<Skill> getSkills(){
+//        return skills;
+//    }
 
     @GetMapping("/new")
     public String showForm(Model model) {
