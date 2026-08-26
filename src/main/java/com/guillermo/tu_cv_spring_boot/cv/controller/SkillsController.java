b011d1faae87668b72b0db25b8bd4ff4896d509e
Guillermo Mora <guillermo_moram@outlook.es>
2026-08-25 21:22:04 -0600

    feat: agregar skills

--- src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/SkillsController.java
+++ src/main/java/com/guillermo/tu_cv_spring_boot/cv/controller/SkillsController.java
@@ -1,4 +1,23 @@
 package com.guillermo.tu_cv_spring_boot.cv.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class SkillsController {
+
+    @GetMapping("/skills")
+    public String skills(Model model) {
+        List<String> skills = List.of("Java", "Spring Boot", "Thymeleaf", "HTML", "CSS");
+        List<String> list = new ArrayList<>();
+
+        model.addAttribute("skills", list);
+
+        return "skills";
+    }
+
 }
